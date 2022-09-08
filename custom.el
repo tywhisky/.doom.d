c35c6ccc5964365a4bead94580049a802d91fcf1
tywhisky <fty221@gmail.com>
2022-09-08 18:32:59 +0800

    Add 2048

--- custom.el
+++ custom.el
@@ -3,7 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(random-splash-image)))
+ '(package-selected-packages '(2048-game)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
