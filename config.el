5570887cef394d16a52c9c7030d74f215a469030
tywhisky <fty221@gmail.com>
2023-12-02 21:41:23 +0800

    Split config file

--- config.el
+++ config.el
@@ -1,5 +1,7 @@
 ;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
 
+(load! "lisp/wsl")
+
 ;; Place your private configuration here! Remember, you do not need to run 'doom
 ;; sync' after modifying this file!
 
@@ -74,34 +76,6 @@
 ;;
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
-
-(defun wsl-copy-region-to-clipboard (start end)
-  "Copy region to Windows clipboard."
-  (interactive "r")
-  (call-process-region start end "clip.exe" nil 0))
-
-(defun wsl-cut-region-to-clipboard (start end)
-  (interactive "r")
-  (call-process-region start end "clip.exe" nil 0)
-  (kill-region start end))
-
-(defun wsl-clipboard-to-string ()
-  "Return Windows clipboard as string."
-  (let ((coding-system-for-read 'dos))
-(substring; remove added trailing \n
- (shell-command-to-string
-  "powershell.exe -Command Get-Clipboard") 0 -1)))
-
-(defun wsl-paste-from-clipboard (arg)
-  "Insert Windows clipboard at point. With prefix ARG, also add to kill-ring"
-  (interactive "P")
-  (let ((clip (wsl-clipboard-to-string)))
-(insert clip)
-(if arg (kill-new clip))))
-
-(define-key global-map (kbd "C-x C-y") 'wsl-paste-from-clipboard)
-(define-key global-map (kbd "C-x M-w") 'wsl-copy-region-to-clipboard)
-(define-key global-map (kbd "C-x C-w") 'wsl-cut-region-to-clipboard)
 
 (defun foo ()
   (interactive)
